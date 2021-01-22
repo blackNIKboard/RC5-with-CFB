8c0a7438c3ede3f579a67641456e1372143c0364
Nikita Maralov <blacknikboard@gmail.com>
2021-01-22 10:48:23 +0300

    fix: Main cleanup

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,24 +5,24 @@ import static main.CypherTester.*;
 
 public class Main {
     public static void main(String[] args) {
-//        testECBImage();
-//        System.out.println("------------------");
-//
-//        testCFBImage();
-//        System.out.println("------------------");
-//
-//        testECBTxt();
-//        System.out.println("------------------");
-//
-//        testCFBTxt();
-//        System.out.println("------------------");
-//
-//        testCFB(5);
-//        System.out.println("------------------");
+        testECBImage();
+        System.out.println("------------------");
+
+        testCFBImage();
+        System.out.println("------------------");
+
+        testECBTxt();
+        System.out.println("------------------");
+
+        testCFBTxt();
+        System.out.println("------------------");
+
+        testCFB(5);
+        System.out.println("------------------");
 
         testCorrelation();
         System.out.println("------------------");
 
-//        errorDistribution(3);
+        errorDistribution(3);
     }
 }
